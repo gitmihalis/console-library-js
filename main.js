7b4bc65fc4917f34c544dda84480bbe9e56e4ae5
gitmihalis <mihaliswastaken@protonmail.com>
2025-07-19 22:57:06 -0400

    add instructions

---
+++ main.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+function displayBooks() {
+  // loop through the array and display each book
+}
+
