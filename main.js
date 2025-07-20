d71ff450384fa2d472e0577ade2389f068522746
gitmihalis <mihaliswastaken@protonmail.com>
2025-07-19 23:41:59 -0400

    displayBooks adds books to the page

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const myLibrary = [];
+const myLibrary = [
+  // this is an array to hold all the books
+  { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, read: true, id: crypto.randomUUID() },
+  { title: "1984", author: "George Orwell", pages: 328, read: false, id: crypto.randomUUID() },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, read: true, id: crypto.randomUUID() },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, read: false, id: crypto.randomUUID() }
+];
 
 function Book(title, author, pages, read=false) {
   // the constructor...
@@ -17,7 +23,9 @@ function addBookToLibrary(book) {
 function displayBooks() {
   // loop through the array and display each book
   for (const book of myLibrary) {
-    console.log(`Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.read}`);
-  } 
+    const newDiv = document.createElement('div');
+    newDiv.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.read}`;
+    console.log(document.body.append(newDiv));
+  }
 }
 
